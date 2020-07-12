edc27fbb8a29cbebca856f9ee707b4507afa4df5
Simon Goldring <sbjgoldring@gmail.com>
2020-07-12 12:08:09 +0100

    Initial Commit

---
+++ SwiftUIGTFS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIGTFS
+//
+//  Created by Simon Goldring on 7/12/20.
+//  Copyright © 2020 Simon Goldring. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
