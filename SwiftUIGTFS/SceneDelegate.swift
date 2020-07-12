cdaa885903aa5154fd0a52aef3473cae1d9e7767
Simon Goldring <sbjgoldring@gmail.com>
2020-07-12 22:05:08 +0100

    added TripManager

--- SwiftUIGTFS/SceneDelegate.swift
+++ SwiftUIGTFS/SceneDelegate.swift
@@ -20,7 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let shapeManager = GTFSShapeManager()
+        let tripManager = GTFSTripManager()
+        let contentView = ContentView(shapeManager: shapeManager, tripManager: tripManager)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
