884b7eebcc6337cfc8ec54c8287af6d99bf49f84
Simon Goldring <sbjgoldring@gmail.com>
2020-07-30 11:20:54 +0100

    Built first experimental test case of routesPublisher with XCTest expectation

--- SwiftUIGTFSTests/SwiftUIGTFSTests.swift
+++ SwiftUIGTFSTests/SwiftUIGTFSTests.swift
@@ -7,8 +7,11 @@
 //
 
 import XCTest
+import Combine
+@testable import SwiftUIGTFS
 
 class SwiftUIGTFSTests: XCTestCase {
+    var cancellables = Set<AnyCancellable>()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -29,6 +32,30 @@ class SwiftUIGTFSTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
     
+    func testCSVReaderRoutesPublisher() throws {
+        let testRoutesData = """
+route_id,route_short_name,route_long_name,route_desc,route_type,route_url,route_color
+1,YL-S,Antioch to SFIA/Millbrae,,1,http://www.bart.gov/schedules/bylineresults?route=1,FFFF33
+2,YL-N,Millbrae/SFIA to Antioch,,1,http://www.bart.gov/schedules/bylineresults?route=2,FFFF33
+"""
+        let expectation = XCTestExpectation(description: "Test routes publisher")
+        
+        let csvReader = CSVDotSwiftReader()
+        csvReader.routesPublisher(from: testRoutesData).sink(receiveCompletion: { completion in
+            switch completion {
+                
+            case .finished:
+                break
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }) { routes in
+            XCTAssertEqual(routes.count, 2)
+            expectation.fulfill()
+        }
+        .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
 }
