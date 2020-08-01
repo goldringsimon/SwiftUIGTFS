25e8e6c48f55a0d9897556fcd952bb0abd215916
Simon Goldring <sbjgoldring@gmail.com>
2020-08-01 10:28:30 +0100

    Better error handling in MockGTFSUnzipper

--- SwiftUIGTFS/Zip/MockGTFSUnzipper.swift
+++ SwiftUIGTFS/Zip/MockGTFSUnzipper.swift
@@ -13,10 +13,22 @@ class MockGTFSUnzipper: GTFSUnzipper {
     
     func unzip(url: URL) -> Future<UnzippedGTFS, GTFSUnzipError> {
         Future { promise in
-            let routesUrl = Bundle.main.url(forResource: "mbtaRoutes", withExtension: ".txt")!
-            let tripsUrl = Bundle.main.url(forResource: "mbtaTrips", withExtension: ".txt")!
-            let shapesUrl = Bundle.main.url(forResource: "mbtaShapes", withExtension: ".txt")!
-            let stopsUrl = Bundle.main.url(forResource: "mbtaStops", withExtension: ".txt")!
+            guard let routesUrl = Bundle.main.url(forResource: "mbtaRoutes", withExtension: ".txt") else {
+                promise(.failure(GTFSUnzipError.missingFile(file: "routes.txt")))
+                return
+            }
+            guard let tripsUrl = Bundle.main.url(forResource: "mbtaTrips", withExtension: ".txt") else {
+                promise(.failure(GTFSUnzipError.missingFile(file: "trips.txt")))
+                return
+            }
+            guard let shapesUrl = Bundle.main.url(forResource: "mbtaShapes", withExtension: ".txt") else {
+                promise(.failure(GTFSUnzipError.missingFile(file: "shapes.txt")))
+                return
+            }
+            guard let stopsUrl = Bundle.main.url(forResource: "mbtaStops", withExtension: ".txt") else {
+                promise(.failure(GTFSUnzipError.missingFile(file: "stops.txt")))
+                return
+            }
             promise(.success(UnzippedGTFS(routesUrl: routesUrl, tripsUrl: tripsUrl, shapesURL: shapesUrl, stopsURL: stopsUrl)))
         }
     }
